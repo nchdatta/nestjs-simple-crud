d0992904f427e75f23a2f7032d8196e51ea068b0
Nayan Chandra Datta <nayanchdatta02@gmail.com>
2023-02-03 22:14:10 +0600

    Complete CRUD, working nice

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './dto/book.dto';
+import { v4 as uuid } from 'uuid';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+    constructor(private bookService: BookService) { }
+
+    @Get()
+    findAll(): Book[] {
+        return this.bookService.findAllBooks();
+    }
+
+    @Get(":id")
+    find(@Param('id') id): Book {
+        return this.bookService.findBook(id);
+    }
+
+    @Post('/add')
+    add(@Body() book: Book): string {
+        const id = uuid();
+        book.id = id;
+        return this.bookService.addBook(book);
+    }
+
+    @Put('/upadte/:id')
+    update(@Param('id') id, @Body() book: Book): string {
+        return this.bookService.updateBook(id, book);
+    }
+
+    @Delete('/delete/:id')
+    delete(@Param('id') id): string {
+        return this.bookService.deleteBook(id);
+    }
+}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from './dto/book.dto';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+    public books: Book[] = [];
+
+    addBook(book: Book): string {
+        this.books.push(book);
+        return "Book has been succesfully added.";
+    }
+    findBook(id: string): Book {
+        const book = this.books.find(b => b.id === id);
+        return book;
+    }
+    findAllBooks(): Book[] {
+        return this.books;
+    }
+
+    deleteBook(id: string): string {
+        const booksFiltered = this.books.filter(b => b.id !== id);
+        this.books = booksFiltered;
+        return "Book has been deleted.";
+    }
+    updateBook(id: string, book: Book): string {
+        const filtered = this.books.filter(b => b.id !== id);
+        const updated = [...filtered, { id: id, ...book }]
+        this.books = updated;
+        return "Book has been updated.";
+    }
+}
